8776d2828e2fa1f1be9a7319f49fa0eb3c2ce2ed
Unai <unai.heredero@ikasle.aeg.eus>
2023-09-20 13:24:48 +0200

    Terminado

---
+++ index.js
@@ -0,0 +1,62 @@
+import {
+    getData
+} from "./service/service.js";
+import {
+    Ingredients
+} from "./classes/ingredients.js";
+import {
+    Cauldron
+} from "./classes/cauldron.js"
+
+const execute = async () => {
+    try {
+        const data = await getData();
+
+        //creamos ingredientes
+
+        const ingredients = Ingredients.load(data);
+        //showIngredients(ingredients);
+
+        const cauldron = new Cauldron(ingredients);
+
+        const potion1 = cauldron.createPotion("Bear Claws", "Bee",);
+        showPotion(potion1);
+
+        const potion2 = cauldron.createPotion("Chicken's Egg", "Chaurus Eggs");
+        showPotion(potion2);
+
+        const potion3 = cauldron.createPotion("Chaurus Eggs", "Bleeding Crown");
+        showPotion(potion3);
+
+        const potion4 = cauldron.createPotion("Nightshade", "Ectoplasm");        
+        showPotion(potion4);
+
+
+    } catch (error) {
+        //ERRORRRRRR
+        console.log(error.message)
+    }
+}
+
+execute();
+
+const showIngredients = (ingredients) => {
+    const ingredientes = ingredients.ingredients;
+
+    ingredientes.forEach(element => {
+        console.log(element.name);
+        console.log(element.effects);
+        console.log("-----------------------------------------------------------")
+
+    });
+
+}
+
+const showPotion = (potion) => {
+    // console.log(`${potion.name}`);
+    // console.log(`Value:         ${potion.value}`)
+    // console.log(`Weight:        ${potion.weight}`)
+    // console.log(`Time:          ${potion.time}`)
+    console.log("------------------------------------")
+    console.log(potion)
+}
---
+++ service/service.js
@@ -0,0 +1,12 @@
+export const getData = async () => {
+    try
+    {
+        const result = await fetch('https://raw.githubusercontent.com/zsiciarz/skyrim-alchemy-toolbox/master/data/ingredients.json');
+        return result.json();
+    }
+    catch(error)
+    {
+        console.log(error.message)
+    }
+}
+
